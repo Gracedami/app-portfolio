c8a8d4a3ff4f96edaafc18960e6d44731de192bd
Ryan Tan <ryantantw95@gmail.com>
2022-12-06 18:09:41 +0800

    app 2

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 // Supports a list of scopes as a string delimited by ',' or ' ' or '%20'
 const SCOPES = (process.env.SCOPE.split(/ |, ?|%20/) || ['crm.objects.contacts.write']).join(' ');
 
-const REDIRECT_URI = `https://lime-naughty-armadillo.cyclic.app/`;
+const REDIRECT_URI = `https://lime-naughty-armadillo.cyclic.app/oauth-callback`;
 const refreshTokenStore = {};
 const accessTokenCache = new NodeCache({ deleteOnExpire: true });
 
